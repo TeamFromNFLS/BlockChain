9311ac63698e31e2d6576ba2a4db42edb1c6c8f8
ge2668 <gyr19@mails.tsinghua.edu.cn>
2020-04-01 22:56:26 +0800

    rsa

--- Project/encryption/RSA/rsa.cpp
+++ Project/encryption/RSA/rsa.cpp
@@ -1,33 +1,38 @@
 #include "rsa.h"
 #include "bigInt.h"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <stdlib.h>
+#include <ctime>
+using namespace std;
 
-//Input1: an integer to be tested
-//Input2: a parameter which determines the accuracy
 bool RSA::IsPrime(const BigInt &num, const int k = 100)
 {
     BigInt Zero("0"), One("1"), Two("2");
-    if (num == Zero)
+    BigInt n = num;
+    if (n == Zero)
         return false;
-    else if (num == Two)
+    else if (n == Two)
         return true;
     else
     {
-        BigInt numMinus = num - One, d, a;
+        BigInt numMinus = n - One, d, a;
         d = numMinus;
         int r = 0;
         //write num-1 as 2^r * d
         while (d % Two == Zero)
         {
             r++;
-            d /= BigInt Two;
+            d /= Two;
         }
         //test if there exists a number a such that a^d mod num != 1
         //and a^(2^j*d) mod num != -1
         for (int i = 0; i < k; i++)
         {
-            a = CreateRandomNum(num); //create a random number between 1 and num
+            a = CreateRandomNum(n); //create a random number between 1 and num
             //test a^d mod num
-            BigInt reMod = BigInt::PowMod(a, d, num);
+            BigInt reMod = BigInt::PowMod(a, d, n);
             if (reMod == One) //probably prime
                 continue;
 
@@ -36,7 +41,7 @@ bool RSA::IsPrime(const BigInt &num, const int k = 100)
             {
                 if (reMod != numMinus) //composite
                     return false;
-                reMod = BigInt::PowMod(reMod, 2, num);
+                reMod = BigInt::PowMod(reMod, Two, n);
             }
         }
         return true;
@@ -54,16 +59,16 @@ BigInt RSA::CreateOdd()
         ss << digit[rand() % 10];
     }
     ss << digit[2 * (rand() % 5) + 1];
-
-    return BigInt Odd(ss.str());
+    BigInt Odd(ss.str());
+    return Odd;
 }
 
 BigInt RSA::CreatePrime()
 {
-    BigInt Prime = CreateOdd();
-    while (!IsPrime(Odd))
+    BigInt Prime = CreateOdd(), Two("2");
+    while (!IsPrime(Prime))
     {
-        Prime += BigInt Two("2"); //if not prime, then plus 2
+        Prime += Two; //if not prime, then plus 2
     }
     return Prime;
 }
