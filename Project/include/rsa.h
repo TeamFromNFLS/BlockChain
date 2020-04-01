0f7c9a13a27f213c1b045c048a72778e5f59cd20
ge2668 <gyr19@mails.tsinghua.edu.cn>
2020-04-01 20:37:40 +0800

    unfinished rsa

--- Project/include/rsa.h
+++ Project/include/rsa.h
@@ -1,23 +1,27 @@
 #ifndef RSA_H
 #define RSA_H
-#include<iostream>
-#include"bigInt.h"
+#include <iostream>
+#include "bigInt.h"
 using namespace std;
 
-class RSA{
-    public:
-        RSA(){}
-        ~RSA(){}
-        void init(); //prime & euler
-        BigInt CreatePrime(); //create two prime numbers p and q
-        BigInt CreatePublic(const BigInt &); //randomly create the public key
-        BigInt CreatePrivate(const BigInt &); //create the private key
-        BigInt encrypt(const BigInt &);
-        BigInt decrypt(const BigInt &);
-        bool IsPrime(const BigInt &);
-        BigInt PublicKey;
-    private:
-        BigInt PrivateKey, p, q, eul;
+class RSA
+{
+public:
+    RSA() {}
+    ~RSA() {}
+    void Init();                            //prime & euler
+    BigInt CreatePrime();                   //create two prime numbers p and q
+    BigInt CreateOdd();                     //create an odd number (and then test if it is prime)
+    BigInt CreatePublic(const BigInt &);    //randomly create the public key
+    BigInt CreateRandomNum(const BigInt &); //for the private key and the prime test
+    BigInt Encrypt(const BigInt &);
+    BigInt Decrypt(const BigInt &);
+    bool IsPrime(const BigInt &, const int k = 100); //k随便取的，判断正确率是1/4^k
+    const int length = 10;
+    BigInt PublicKey;
+
+private:
+    BigInt PrivateKey, p, q, eul;
 };
 
 #endif
