91e81f27b531ac7e3ba32ae9800d42ed9187f6de
ge2668 <gyr19@mails.tsinghua.edu.cn>
2020-04-02 11:07:19 +0800

    modify

--- Project/encryption/RSA/rsa.cpp
+++ Project/encryption/RSA/rsa.cpp
@@ -7,38 +7,48 @@
 #include <ctime>
 using namespace std;
 
-bool RSA::IsPrime(const BigInt &num, const int k = 100)
+void RSA::Init()
 {
-    BigInt n = num;
-    if (n == number.empty())
+    //产生大素数p,q
+    p = CreatePrime();
+    q = CreatePrime();
+    Product = p * q;
+    //欧拉数
+    Euler = (p - 1) * (q - 1);
+}
+
+bool RSA::IsPrime(const BigInt &Num, const int k = 100)
+{
+    BigInt n = Num;
+    if (n == n.number.empty())
         return false;
     else if (n == 2)
         return true;
     else
     {
-        BigInt numMinus = n - 1, d, a;
-        d = numMinus;
+        BigInt NumMinus = n - 1, d, a;
+        d = NumMinus;
         int r = 0;
-        //write num-1 as 2^r * d
-        while (d % 2 == number.empty())
+        //write Num-1 as 2^r * d
+        while (d % 2 == n.number.empty())
         {
             r++;
             d /= 2;
         }
-        //test if there exists a number a such that a^d mod num != 1
-        //and a^(2^j*d) mod num != -1
+        //test if there exists a Number a such that a^d mod Num != 1
+        //and a^(2^j*d) mod Num != -1
         for (int i = 0; i < k; i++)
         {
-            a = CreateRandomNum(n); //create a random number between 1 and num
-            //test a^d mod num
+            a = CreateRandomSmaller(n); //create a random Number between 1 and Num
+            //test a^d mod Num
             BigInt reMod = BigInt::PowMod(a, d, n);
             if (reMod == 1) //probably prime
                 continue;
 
-            //test a^(2^j*d) mod num for 0 <= j <= r-1
+            //test a^(2^j*d) mod Num for 0 <= j <= r-1
             for (int j = 0; j <= r - 1; j++)
             {
-                if (reMod != numMinus) //composite
+                if (reMod != NumMinus) //composite
                     return false;
                 reMod = BigInt::PowMod(reMod, 2, n);
             }
@@ -47,27 +57,82 @@ bool RSA::IsPrime(const BigInt &num, const int k = 100)
     }
 }
 
-BigInt RSA::CreateOdd()
+BigInt RSA::CreateRandom(int isOdd)
 {
     const int digit[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     stringstream ss;
 
     srand((unsigned)time(NULL)); //generate seed
-    for (int i = 0; i < length - 1; i++)
+    switch (isOdd)
     {
-        ss << digit[rand() % 10];
+    case 1: //create random odd
+        for (int i = 0; i < length - 1; i++)
+        {
+            ss << digit[rand() % 10];
+        }
+        ss << digit[2 * (rand() % 5) + 1];
+        break;
+
+    case 0: //create random
+        for (int i = 0; i < length; i++)
+        {
+            ss << digit[rand() % 10];
+        }
+        break;
     }
-    ss << digit[2 * (rand() % 5) + 1];
-    BigInt Odd(ss.str());
-    return Odd;
+
+    BigInt Random(ss.str());
+    return Random;
 }
 
 BigInt RSA::CreatePrime()
 {
-    BigInt Prime = CreateOdd(), 2("2");
+    BigInt Prime = CreateRandom(1);
     while (!IsPrime(Prime))
     {
         Prime += 2; //if not prime, then plus 2
     }
     return Prime;
+}
+
+BigInt RSA::CreateRandomSmaller(const BigInt &Num)
+{
+    BigInt n = Num;
+    const int digit[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    stringstream ss;
+    bool zero = true;
+
+    while (zero)
+    {
+        BigInt Random = CreateRandom(0);
+        Random %= Random;
+        if (Random)
+        {
+            zero = false;
+        }
+    }
+}
+
+BigInt RSA::EncryptByPublic(const BigInt &Num)
+{
+    BigInt B4encrypt = Num;
+    BigInt Encrypted = BigInt::PowMod(B4encrypt, PublicKey, Product);
+    return Encrypted;
+}
+
+BigInt RSA::EncryptByPrivate(const BigInt &Num)
+{
+    return DecryptByPrivate(Num);
+}
+
+BigInt RSA::DecryptByPrivate(const BigInt &Num)
+{
+    BigInt B4decrypt = Num;
+    BigInt Decrypted = BigInt::PowMod(B4decrypt, PrivateKey, Product);
+    return Decrypted;
+}
+
+BigInt RSA::DecryptByPublic(const BigInt &Num)
+{
+    return EncryptByPublic(Num);
 }
--- Project/include/rsa.h
+++ Project/include/rsa.h
@@ -9,19 +9,21 @@ class RSA
 public:
     RSA() {}
     ~RSA() {}
-    void Init();                            //prime & euler
-    BigInt CreatePrime();                   //create two prime numbers p and q
-    BigInt CreateOdd();                     //create an odd number (and then test if it is prime)
-    BigInt CreatePublic(const BigInt &);    //randomly create the public key
-    BigInt CreateRandomNum(const BigInt &); //for the private key and the prime test
-    BigInt Encrypt(const BigInt &);
-    BigInt Decrypt(const BigInt &);
+    void Init();                                //prime & euler
+    BigInt CreatePrime();                       //create two prime numbers p and q
+    BigInt CreateRandom(int);                   //create an odd number (and then test if it is prime)
+    BigInt CreatePublic(const BigInt &);        //randomly create the public key
+    BigInt CreateRandomSmaller(const BigInt &); //for the private key and the prime test
+    BigInt EncryptByPublic(const BigInt &);
+    BigInt EncryptByPrivate(const BigInt &);
+    BigInt DecryptByPublic(const BigInt &);
+    BigInt DecryptByPrivate(const BigInt &);
     bool IsPrime(const BigInt &, const int k = 100); //k随便取的，判断错误率是1/4^k
     const int length = 10;
-    BigInt PublicKey;
+    BigInt PublicKey, Product;
 
 private:
-    BigInt PrivateKey, p, q, eul;
+    BigInt PrivateKey, p, q, Euler;
 };
 
 #endif
