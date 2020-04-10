96cd71004f8b2691023f6bd6f6551c9e1167f02c
henry-ph <910372168@qq.com>
2020-04-10 21:41:33 +0800

    put

---
+++ Project/main/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include "bigInt.h"
+#include "rsa.h"
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+void TestRandom();
+int main()
+{
+     RSA a;
+     a.Init();
+     //TestRandom();
+     return 0;
+}
