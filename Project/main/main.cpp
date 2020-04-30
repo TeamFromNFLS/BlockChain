6bf26dfae07328fea33714ac9ef200e49a6c6dac
henry-ph <910372168@qq.com>
2020-04-30 19:38:23 +0800

    main

--- Project/main/main.cpp
+++ Project/main/main.cpp
@@ -13,7 +13,7 @@
 using namespace std;
 
 const int INF = 0x3f3f3f3f;
-set<string> commendSet{"demo", "help", "add", "delete", "wallet", "miner", "find", "make", "exist", "exit"};
+set<string> commendSet{"demo", "help", "add", "delete", "wallet", "miner", "find", "make", "check", "exit"};
 
 string EditDistance(string &s)
 {
@@ -64,12 +64,25 @@ string EditDistance(string &s)
 int main()
 {
      struct timeval timeStart, timeEnd;
-     //balance();
-     //TestMine();
+     cout << "Welcome to Prometheus, a blockchain simulation program." << endl
+          << "For bug reporting instructions, please see:" << endl
+          << "N/A" << endl
+          << "For help, type \"help\"." << endl;
      string cmd;
-     while (cin >> cmd)
+     while (getline(cin, cmd))
      {
-          set<string>::iterator it = commendSet.find(cmd);
+          cmd.insert(cmd.end(), ' ');
+          vector<string> cmdList;
+          int pos = 0;
+          for (int i = 0; i < cmd.length(); ++i)
+          {
+               if (cmd[i] == ' ')
+               {
+                    cmdList.push_back(cmd.substr(pos, i - pos));
+                    pos = i + 1;
+               }
+          }
+          set<string>::iterator it = commendSet.find(cmdList[0]);
           if (it != commendSet.end())
           {
                if (*it == "demo")
@@ -78,7 +91,7 @@ int main()
                     cout << "Run demo..." << endl;
                     double runtime = 0;
                     gettimeofday(&timeStart, NULL);
-                    balance();
+                    //balance();
                     gettimeofday(&timeEnd, NULL);
                     runtime = (timeEnd.tv_sec - timeStart.tv_sec) + (double)(timeEnd.tv_usec - timeStart.tv_usec) / 1000000;
                     cout << "Total time: " << runtime << "s." << endl;
@@ -87,10 +100,27 @@ int main()
                }
                if (*it == "help")
                {
-                    cout << "List of classes of commands:" << endl
-                         << "add -- add a wallet or miner." << endl
-                         << endl
-                         << "Type \"help\" followed by command name for full documentation." << endl;
+                    if (cmdList.size() == 1)
+                    {
+                         cout << "List of classes of commands:" << endl
+                              << "demo -- run demo." << endl
+                              << "add -- add a wallet or miner." << endl
+                              << "delete -- delete a wallet or miner." << endl
+                              << "find -- find the balance of a wallet." << endl
+                              << "make -- make a transaction." << endl
+                              << "check -- check whether the wallet contains a right blockchain." << endl
+                              << "exit -- exit the program." << endl
+                              << endl
+                              << "Type \"help\" followed by command name for full documentation." << endl;
+                    }
+                    else
+                    {
+                         if (commendSet.find(cmdList[1]) == commendSet.end())
+                         {
+                              string guess = EditDistance(cmdList[1]);
+                              cout << "Undefined command: \"" << cmdList[1] << "\". Do you mean \"" << guess << "\" ?" << endl;
+                         }
+                    }
                }
                if (*it == "exit")
                {
