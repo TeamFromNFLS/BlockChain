38ce0de34ce1f92c7554ff6527aad7f5e085b69a
henry-ph <910372168@qq.com>
2020-05-13 17:26:59 +0800

    main

--- Project/include/command.h
+++ Project/include/command.h
@@ -10,7 +10,6 @@ using namespace std;
 
 enum COMMAND
 {
-    DEMO,
     HELP,
     ADD,
     DELETE,
--- Project/main/main.cpp
+++ Project/main/main.cpp
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <termios.h>
 #include "command.h"
 
 using namespace std;
@@ -8,6 +11,28 @@ enum CMD_SET
      DEBUG,
      ERR
 };
+
+/*read input from keyboard.
+版权声明：本文为CSDN博主「jieniyimiao」的原创文章，遵循CC 4.0 BY-SA版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/u013467442/java/article/details/51173441/*/
+int scanKeyboard()
+{
+     int in;
+     struct termios new_settings;
+     struct termios stored_settings;
+     tcgetattr(0, &stored_settings);
+     new_settings = stored_settings;
+     new_settings.c_lflag &= (~ICANON);
+     new_settings.c_cc[VTIME] = 0;
+     tcgetattr(0, &stored_settings);
+     new_settings.c_cc[VMIN] = 1;
+     tcsetattr(0, TCSANOW, &new_settings);
+
+     in = getchar();
+
+     tcsetattr(0, TCSANOW, &stored_settings);
+     return in;
+}
 
 CMD_SET CmdSelect(string const cmd)
 {
@@ -65,7 +90,7 @@ int main(int argc, char **argv)
           }
           switch (CmdSelect(argv[1]))
           {
-          case SHOW:
+          case DEMO:
           {
                cout << "Run demo..." << endl;
                fileIn.open("demo.txt", ios::in);
@@ -105,6 +130,10 @@ int main(int argc, char **argv)
      }
      catch (int)
      {
+          while (1)
+          {
+               printf(":%d", scanKeyboard());
+          }
           string cmd;
           while (getline(cin, cmd))
           {
